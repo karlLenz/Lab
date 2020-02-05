89f8d42d8fb0301160ca3ebe15ac85437d1a82fd
Laig Gasanov <karllenzpa@gmail.com>
2020-02-05 15:22:05 +0700

    refactoring(TreapTesting.h) переделаны функции тестирования вставки и удаления Treap

--- Lab5NonBalancingBtrees/TreapTesting.h
+++ Lab5NonBalancingBtrees/TreapTesting.h
@@ -4,24 +4,14 @@
 
 
 template <typename U>
-void TestAdding(Treap<U> &treap, void (Treap<U>::*AddFunction)(U),
+void Test(Treap<U> &treap, void (Treap<U>::*TestingFunction)(U),
 	size_t elementCount)
 {
 	Timer time;
 	for (size_t i = 0; i < elementCount; ++i)
 	{
-		(treap.*AddFunction)(static_cast<short>(i));
+		(treap.*TestingFunction)(static_cast<short>(i));
 	}
-}
-
-
-template <typename U>
-void TestRemoving(Treap<U> &treap, void (Treap<U>::*RemoveFunction)(U))
-{
-	short someKey = rand();
-	treap.AddOptimized(someKey);
-	Timer time;
-	(treap.*RemoveFunction)(someKey);
 }
 
 
@@ -29,17 +19,16 @@ void Testing(size_t elementCount)
 {
 	Treap<short> treap;
 	std::cout << "Время неоптимизированной вставки: ";
-	TestAdding(treap, &Treap<short>::AddNonOptimized, elementCount);
+	Test(treap, &Treap<short>::AddNonOptimized, elementCount);
 	std::cout << " (высота дерева = " << treap.GetHeight() << ")\n";
-	treap.~Treap();
+
+	std::cout << "\nВремя неоптимизированного удаления добавленных элементов: ";
+	Test(treap, &Treap<short>::RemoveNonOptimized, elementCount);
 
 	std::cout << "Время оптимизированной вставки: ";
-	TestAdding(treap, &Treap<short>::AddOptimized, elementCount);
+	Test(treap, &Treap<short>::AddOptimized, elementCount);
 	std::cout << " (высота дерева = " << treap.GetHeight() << ")\n";
 
-	std::cout << "\nВремя неоптимизированного удаления: ";
-	TestRemoving(treap, &Treap<short>::RemoveNonOptimized);
-
-	std::cout << "\nВремя оптимизированного удаления: ";
-	TestRemoving(treap, &Treap<short>::RemoveOptimized);
+	std::cout << "\nВремя оптимизированного удаления добавленных элементов: ";
+	Test(treap, &Treap<short>::RemoveOptimized, elementCount);
 }
